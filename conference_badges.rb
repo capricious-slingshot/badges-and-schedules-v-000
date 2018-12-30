3fc4ecb5644796072ed5cc95a1e4b12cb7f2bd19
Carden Z <ephemere@protonmail.com>
2018-12-30 04:56:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,12 @@ def batch_badge_creator(array)
 	end
 	badge_messages
 end
+
+def assign_rooms(speakers)
+	assignments = []
+	speakers.each_with_index do |name|
+		room = speaker[name] + 1
+		assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
+	end
+	assignments
+end
