cdb281158c14930de41b51c76b993ec6fc446ff9
Carden Z <ephemere@protonmail.com>
2018-12-30 04:51:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,10 @@ def badge_maker(name)
 	"Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-	
+def batch_badge_creator(array)
+	badge_messages = []
+  array.each do |name|
+    badge_messages << badge_maker(name)
+	end
+	badge_messages
 end
